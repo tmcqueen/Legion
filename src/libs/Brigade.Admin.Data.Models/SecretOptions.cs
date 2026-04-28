7c3ab4b702ae9d7217df2070d9217f69436402ec
Tim McQueen <tim.mcqueen@gmail.com>
2026-04-28 05:07:28 -0500

    feat: add SecretOptions model and EF configuration

---
+++ src/libs/Brigade.Admin.Data.Models/SecretOptions.cs
@@ -0,0 +1,11 @@
+namespace Brigade.Admin.Data.Models;
+
+public record SecretOptions
+{
+    public int Id { get; init; }
+    public string Path { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string EncryptedValue { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
